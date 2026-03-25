023c7e4e1a61d86c1f6ff5a881e2f9fbf0488cca
Hugo SC <hugosc@users.noreply.github.com>
2026-03-25 23:06:44 +0000

    feat: line breaks

--- collab.c
+++ collab.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("boy i love C");
-    printf("poop");
-    printf("rumplestiltskin");
-    printf("jujutsu kasen");
-    printf("pee");
-    printf("Very professional. I hope this won't happen again at our company.");
+    printf("boy i love C\n");
+    printf("poop\n");
+    printf("rumplestiltskin\n");
+    printf("jujutsu kasen\n");
+    printf("pee\n");
+    printf(
+        "Very professional. I hope this won't happen again at our company.\n");
 }
